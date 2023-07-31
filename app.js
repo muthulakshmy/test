e88c98770b1edd7235e81cbae10fbe4c050e1ec7
Lakshmy <88577608+muthulakshmy@users.noreply.github.com>
2023-07-31 14:42:23 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-let notes=[];
+document.addEventListener("DOMContentLoaded",add_notes)
+let popup=document.getElementById("popup")
+let notes=JSON.parse(localStorage.getItem('note'))||[]
 function add_notes(){
     let notes_title=document.getElementById("text").value
     console.log(notes_title)
     let notes_des=document.getElementById("textarea").value
     console.log(notes_des)
-    if(notes_title&&notes_des){
+    if(notes_title && notes_des){
         let note={
             title:notes_title,
             des:notes_des
@@ -16,7 +18,7 @@ function add_notes(){
         }else{
             notes.push(note)
             console.log(notes)
-
+            localStorage.setItem("note",JSON.stringify(notes))
         }
         display_notes()
     }
@@ -30,7 +32,7 @@ function display_notes(){
     note_list.innerHTML=""
     notes.forEach((note)=>{
         let note_display=document.createElement('li');
-        note_display.innerHTML=`${note.title}<br>${note.des}<br>`;
+        note_display.innerHTML=`<h1 id="header">${note.title}</h1>`;
         let editButton=document.createElement("button")
         editButton.innerText="Edit";
         editButton.setAttribute("id","edit")
@@ -43,8 +45,49 @@ function display_notes(){
         note_display.appendChild(editButton)
         note_display.appendChild(deleteButton)
         note_list.appendChild(note_display)
-        
-    })
+        let head = note_display.querySelector("#header");
+        head.removeEventListener("click", pop); 
+
+    
+        head.addEventListener("click", pop); 
+        let display_title = null; 
+
+function pop() {
+  let title = this.innerText;
+  let note = notes.find((value) => value.title === title);
+
+  if (display_title === note) {
+    popup_hide(); 
+  } else {
+    popup_display(note); 
+  }
+}
+
+function popup_display(note) {
+  display_title = note; 
+
+  let para = document.createElement('p');
+  para.setAttribute("id", "para");
+  para.innerHTML = note.des; 
+
+  let cancel = document.createElement('button');
+  cancel.setAttribute("id", "cancel");
+  cancel.innerHTML = "cancel";
+  cancel.addEventListener("click", popup_hide);
+
+  popup.appendChild(para);
+  para.appendChild(cancel);
+  popup.style.display = "block";
+}
+
+function popup_hide() {
+    para.remove()
+  popup.style.display = "none";
+  display_title = null; 
+}
+
+})
+
 }
 function editNote(note){
     document.getElementById("text").value=note.title;
@@ -75,9 +118,6 @@ function clear_notes(){
     document.getElementById("text").value="";
     document.getElementById("textarea").value="";
     document.getElementById("add_notes").innerText="save notes"
-    document.getElementById("add_notes").onclick=()=>saveNote;
+    document.getElementById("add_notes").onclick=()=>add_notes()
 
 }
-
-
-
